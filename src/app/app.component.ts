d5fb1de5f32d4090d69500df8b45eafc358da0b7
Burton Smith <burton@breakstuff.io>
2025-07-15 22:14:04 -0400

    add examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  forwardRef,
+  input,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, forwardRef(() => AppTestComponent)],
+  // templateUrl: './app.component.html',
+  template: `
+    <h1>{{ title }}</h1>
+    <app-test [title]="3"></app-test>
+    <my-element></my-element>
+    <router-outlet></router-outlet>
+  `,
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'angular-wc-test';
+}
+
+@Component({
+  selector: 'app-test',
+  imports: [RouterOutlet],
+  template: `
+    <h1>{{ title }}</h1>
+    <p>{{ value }}</p>
+  `,
+  styleUrl: './app.component.css',
+})
+export class AppTestComponent {
+  title = 'angular-wc-test';
+  value = input.required<number>();
 }
